2f004ffb3833ddc2d123394f5a2ff4896dc6b127
Dave <davue@bluewin.ch>
2016-04-08 15:30:14 +0200

    restructured whole bot

---
+++ lib/core.lua
@@ -0,0 +1,50 @@
+--[[ Docs:
+The command library contains the chat command handling of the bot
+
+command.Add(string: command, function: cb_func)
+	Adds a chat command which can be called via !<command>.
+	The callback function gets 2 arguments:
+		- msg: The message table of the message which triggered the command
+		- args[]: An array of arguments passed to the command
+	
+command.Remove(string: command)
+	Removes a chat command. The chat command will be unknown after removing it.
+	
+command.Call(string: command, table: msg, table: args)
+	Calls a chat command from inside lua.
+]]
+
+command = {} -- Table containing the commmand library
+local chatCommands = {} -- chatCommands[command] = function of command
+
+function command.Add(command, func)
+	chatCommands[command] = func
+end
+
+function command.Remove(command)
+	chatCommands[command] = nil
+end
+
+function command.Call(command, msg, args)
+	chatCommands[command](msg, args)
+end
+
+local function handleMessage(msg)
+	if(msg.text ~= nil) then -- If there is actually text
+		if(string.sub(msg.text,1,1) == "!") then -- If it starts like a command
+			local args = {}
+			for subStr in string.gmatch(string.sub(msg.text,2),"%S+") do table.insert(args, subStr) end -- Split message by spaces and remove command sign
+			local command = args[1]
+			table.remove(args, 1) -- Remove first element of arguments because it's the command itself
+		
+			if(chatCommands[command] ~= nil) then -- If command exists
+				chatCommands[command](msg, args) -- Call command with msg and arguments
+				deleteMessage(msg.channel.id, msg.id) -- Remove command message
+			else
+				sendMessage(msg.channel.id, "[INFO] Unknown command.")
+			end
+		end
+	end
+end
+
+hook.Add("onMessageReceived", "messageHandler", handleMessage) -- Register message handler
--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-chatCommands = {} -- chatCommands[command] = function of command
 botName = "S-Bot"
 installPath = "/home/Dave/discord/lua"
 defaultFilePath = installPath.."/main.lua"
@@ -8,147 +7,9 @@ mainChannel = "165560868426219520" -- #stammbot-dev-channel @ Stammgruppe Afterb
 
 local admins = {"163605020271575041"} -- Dave-it
 
--- Events
-function onMessageReceived(msg)
-	hook.Call("onMessageReceived", msg)
-end
-
-function onLuaError(reason)
-	sendMessage(mainChannel, "An error occured while running the script:\n"..reason)
-end
-
-function onPortData(data)
-	sendMessage(mainChannel, data)
-end
-
--- Core functions
-function addCommand(command, func)
-	chatCommands[command] = func
-end
-
-function handleMessage(msg)
-	if(msg.text ~= nil) then -- if there is actually text
-		if(string.sub(msg.text,1,1) == "!") then -- if it starts like a command
-			local args = {}
-			for subStr in string.gmatch(string.sub(msg.text,2),"%S+") do table.insert(args, subStr) end -- split message by spaces and remove command sign
-			local command = args[1]
-			table.remove(args, 1) -- remove first element of arguments because it's the command itself
-		
-			if(chatCommands[command] ~= nil) then -- if command exists
-				chatCommands[command](msg, args) -- call command with msg and arguments
-				deleteMessage(msg.channel.id, msg.id) -- remove command message
-			else
-				sendMessage(msg.channel.id, "["..botName.."] Unknown command.")
-			end
-		end
-	end
-end
-
--- WARNING: this function is super chaotic and non-readable
--- needs proper recoding
-function handleMessageOld(msg)
-	if(msg.text ~= nil) then
-		if(string.sub(msg.text,1,1) == "!") then
-			local subStrings = {}
-			for subStr in string.gmatch(string.sub(msg.text,2),"%S+") do table.insert(subStrings, subStr) end -- Split message
-			
-			local args = {}
-			args = deepcopy(subStrings) -- Make a true copy of array
-			table.remove(args,1) -- Remove first element, since it's the command itself
-			
-			local qArgs = {}
-			
-			local quoteClosed = true
-			local skipTo = 1
-			
-			for k, v in pairs(args) do -- Quotes parsen
-				if(string.sub(v, -1) == "\"" and string.sub(v, 1, 1) == "\"") then -- Wenn Anfangsquote auch Endquote ist
-					table.insert(qArgs, string.sub(v, 2, -2))
-				elseif(string.sub(v, 1, 1) == "\"") then -- Wenn Anfangsquote gefunden wurde
-					quoteClosed = false
-					for i=k+1,#args do -- Durch alle restlichen Argumente gehen und Endquote suchen
-						if(string.sub(args[i], -1) == "\"") then -- Wenn Endquote gefunden wurde
-							quoteClosed = true -- Quote wurde geschlossen
-							local quoteArg = ""
-							quoteArg = quoteArg..string.sub(v, 2) -- Argument mit Anfangsquote zu quoteArg hinzufügen
-							for j=k+1,i-1 do -- Von Anfangs+1 bis Endquote-1 durchgehen
-								quoteArg = quoteArg.." "..args[j] -- Argumente zwischen Anfangs und Endquote zu quoteArg hinzufügen
-							end
-							quoteArg = quoteArg.." "..string.sub(args[i], 1, -2) -- Argument mit Endquote zu quoteArg hinzufügen
-							skipTo = i+1 -- Bis nach die Quote skippen
-							table.insert(qArgs, quoteArg) -- quoteArg in neues Argument-Table hinzufügen
-							break
-						end
-					end
-				else
-					if(k >= skipTo) then -- Erst wenn loop nach der letzten Quote ist wieder einzelne Elemente inserten
-						table.insert(qArgs, v)
-					end
-				end
-			end
-
-			local commandExist = false
-			local command = string.lower(subStrings[1])
-			for k, v in pairs(chatCommands) do
-				if(chatCommands[command] ~= nil) then
-					commandExist = true
-					if (quoteClosed or command == "lua" or command == "luas" or command == "sh") then -- Lua and Shell command should ignore quotes
-						chatCommands[command](msg, qArgs)
-					else -- If there are unclosed quotes
-						sendMessage(msg.channel.id, "["..botName.."] Error: Not every quote is closed!")
-					end
-					break
-				end
-			end
-			
-			if (not commandExist) then -- If command wasn't existing
-				sendMessage(msg.channel.id, "["..botName.."] Unknown command.")
-			end
-		end
-	end
-end
-
--- Checks if sender is admin
-function isAdmin(msg)
-	for k, v in pairs(admins) do
-		if(msg.author.id == v) then
-			return true
-		end
-	end
-	sendMessage(msg.channel.id, "["..botName.."] Admin-Only Command")
-	return false
-end
-
--- Vital chat commands
-addCommand("update", function(msg, args)
-	if(isAdmin(msg)) then
-		os.execute("git -C ".. installPath .." reset --hard")
-		local text = os.capture("git -C ".. installPath .." pull")
-		if (text ~= "Already up-to-date.") then
-			local beginPos, endPos, fromVersion, toVersion = string.find(text, "(%w+)%.%.(%w+)") 	-- Get version hashes
-			text = string.sub(text, endPos+15)														-- Remove version hashes from string
-			text = string.gsub(text, "([%+%-]+)%s", "%1\n")											-- Format file changes
-			sendMessage(mainChannel, "[Update] Updating from <".. fromVersion .."> to <".. toVersion .. ">\n"..text)
-			sendMessage(mainChannel, "[Update] Please reload the bot to apply the changes.")		-- Safety delay to give the update process some time (needs admin credentials)
-		else
-			sendMessage(mainChannel, "[Update] Already up-to-date.")
-		end
-	end
-end)
-
-addCommand("reload", function(msg, args)
-	if(isAdmin(msg)) then
-		func, errorStr = loadfile(defaultFilePath)
-		if(func == nil) then
-			sendMessage(mainChannel, "["..botName.."] An error occured while running the script:\n"..errorStr)
-		else
-			func()
-		end
-	end
-end)
-
--- Function to execute "cmd" in the standard command line and returns it's output.
--- is needed to initialize
+------------------------
+---- Initialization ----
+------------------------
 function os.capture(cmd, raw)
 	local f = assert(io.popen(cmd, 'r'))
 	local s = assert(f:read('*a'))
@@ -160,8 +21,7 @@ function os.capture(cmd, raw)
 	return s
 end
 
------- Load Libraries ------
-function loadLibs()
+function loadLibs() -- Load Libraries
 	lsStr = os.capture("ls "..libPath)
 	local libs = {}
 	for file in string.gmatch(lsStr, "%a+.lua") do 
@@ -175,8 +35,7 @@ function loadLibs()
 	end
 end
 
------- Load Modules ------
-function loadModules()
+function loadModules() -- Load Modules
 	lsStr = os.capture("ls "..modulePath)
 	local modules = {}
 	for file in string.gmatch(lsStr, "%a+.lua") do 
@@ -193,6 +52,49 @@ end
 loadLibs()			-- Load essential libraries
 loadModules()		-- Load additional modules
 
-sendMessage(mainChannel, "["..botName.."] Initialized!")
+sendMessage(mainChannel, "[INFO] Initialized!")
 
-hook.Add("onMessageReceived", "messageHandler", handleMessage)
+-----------------------------
+---- Vital Chat Commands ----
+-----------------------------
+core.addCommand("update", function(msg, args)
+	if(isAdmin(msg)) then
+		os.execute("git -C ".. installPath .." reset --hard")
+		local text = os.capture("git -C ".. installPath .." pull")
+		if (text ~= "Already up-to-date.") then
+			local beginPos, endPos, fromVersion, toVersion = string.find(text, "(%w+)%.%.(%w+)") 	-- Get version hashes
+			text = string.sub(text, endPos+15)														-- Remove version hashes from string
+			text = string.gsub(text, "([%+%-]+)%s", "%1\n")											-- Format file changes
+			sendMessage(mainChannel, "[Update] Updating from <".. fromVersion .."> to <".. toVersion .. ">\n"..text)
+			sendMessage(mainChannel, "[Update] Please reload the bot to apply the changes.")		-- Safety delay to give the update process some time (needs admin credentials)
+		else
+			sendMessage(mainChannel, "[Update] Already up-to-date.")
+		end
+	end
+end)
+
+core.addCommand("reload", function(msg, args)
+	if(isAdmin(msg)) then
+		func, errorStr = loadfile(defaultFilePath)
+		if(func == nil) then
+			sendMessage(mainChannel, "["..botName.."] An error occured while running the script:\n"..errorStr)
+		else
+			func()
+		end
+	end
+end)
+
+----------------
+---- Events ----
+----------------
+function onMessageReceived(msg)
+	hook.Call("onMessageReceived", msg)
+end
+
+function onLuaError(reason)
+	sendMessage(mainChannel, "An error occured while running the script:\n"..reason)
+end
+
+function onPortData(data)
+	sendMessage(mainChannel, data)
+end
