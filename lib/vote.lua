4ee5ab40221d5618c6afd186cfe062d3619d5478
David Enderlin <davue@bluewin.ch>
2016-04-22 16:22:01 +0200

    working on vote lib

--- lib/vote.lua
+++ lib/vote.lua
@@ -2,18 +2,20 @@ vote = {}
 
 local votelist = {}
 
-function vote.start(identifier, callback, ...)
+function vote.start(identifier, printfunc, callback, ...)
 	args = {...}
 	
 	if(#args >= 2) then
-		local voteTable = {}			-- Table containing information about the vote
+		local voteTable = {}				-- Table containing information about the vote
+		voteTable.print = printfunc	-- Set the print function
 		voteTable.callback = callback	-- Set the callback function
 		voteTable.question = args[1]	-- Set the question
 		voteTable.answers = {}
 		table.remove(args, 1)
 		
 		for k, v in pairs(args) do
-			--voteTable.answers[]
+			voteTable.answers[k].name = v
+			voteTable.answers[k].count = 0
 		end
 		
 		votelist[identifier] = voteTable;	-- Insert vote into votelist
