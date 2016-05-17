a791b3411e75d717ba902a7c7d078bdb0dd94156
Dave <davue@bluewin.ch>
2016-05-17 17:53:54 +0200

    fixed volvopls

--- modules/volvo.lua
+++ modules/volvo.lua
@@ -1,9 +1,10 @@
 command.add("volvopls", function(msg, args)
 	if(#args > 0) then
 		local text = string.sub(msg.getContent(), 11)
-		msg.edit("༼ つ ◕_◕ ༽つ "..text)
+		msg.getChannel().sendMessage("༼ つ ◕_◕ ༽つ "..text)
 	else
 		msg.getChannel().sendMessage("[INFO] Usage: volvopls <text>")
-		msg.delete()
 	end
+	
+	msg.delete()
 end)
