bd0eb9dfd55c1fad1b7f97110684d6fd70e2b99d
Dave <davue@bluewin.ch>
2016-05-21 19:15:41 +0200

    removed join and leave from admin commands

--- modules/voice.lua
+++ modules/voice.lua
@@ -204,77 +204,69 @@ command.add("resume", function(msg, args)
 end)
 
 command.add("join", function(msg, args)
-	if(core.isAdmin(msg)) then
-		voiceChannels = msg.getGuild().getVoiceChannels()
-		connectedChannels = discord.getConnectedVoiceChannels()
-		
-		if(#voiceChannels == 1 and #args == 0) then -- if there is only one voice channel -> join this channel
-			if(not voiceChannels[1].isConnected()) then
-				voiceChannels[1].join()
-				connectedChannel = voiceChannels[1]
-			else
-				msg.getChannel().sendMessage("[INFO] I'm already in the voice channel.")
-			end
-		elseif(#voiceChannels >= 1) then -- if there are more than one voice channels
-			if(#args == 1) then -- when there are one or more arguments passed
-				channelExists = false;
-				alreadyInChannel = false;
-				
-				for k, channel in pairs(voiceChannels) do
-					for i, conChannel in pairs(connectedChannels) do
-						if(channel.getID() == conChannel.getID()) then -- if you are already in a channel of the same server
-							channelExists = true
-							alreadyInChannel = true
-							if(channel.getName() == args[1]) then -- if it's the same channel you want to join
-								msg.getChannel().sendMessage("[INFO] I'm already in this voice channel.")
-								break
-							else
-								msg.getChannel().sendMessage("[INFO] I'm already in the voice channel: \"".. channel.getName() .."\".\n[INFO] I can only join one channel per server.")
-								break
-							end
-						end
-					end
-					
-					if(args[1] == channel.getName() and not alreadyInChannel) then -- if there is a channel with name <arg>
-						if(not voiceChannels[k].isConnected()) then
-							voiceChannels[k].join()
-							connectedChannel = voiceChannels[k]
-							channelExists = true
+	voiceChannels = msg.getGuild().getVoiceChannels()
+	connectedChannels = discord.getConnectedVoiceChannels()
+	
+	if(#voiceChannels == 1 and #args == 0) then -- if there is only one voice channel -> join this channel
+		if(not voiceChannels[1].isConnected()) then
+			voiceChannels[1].join()
+			connectedChannel = voiceChannels[1]
+		else
+			msg.getChannel().sendMessage("[INFO] I'm already in the voice channel.")
+		end
+	elseif(#voiceChannels >= 1) then -- if there are more than one voice channels
+		if(#args == 1) then -- when there are one or more arguments passed
+			channelExists = false;
+			alreadyInChannel = false;
+			
+			for k, channel in pairs(voiceChannels) do
+				for i, conChannel in pairs(connectedChannels) do
+					if(channel.getID() == conChannel.getID()) then -- if you are already in a channel of the same server
+						channelExists = true
+						alreadyInChannel = true
+						if(channel.getName() == args[1]) then -- if it's the same channel you want to join
+							msg.getChannel().sendMessage("[INFO] I'm already in this voice channel.")
+							break
+						else
+							msg.getChannel().sendMessage("[INFO] I'm already in the voice channel: \"".. channel.getName() .."\".\n[INFO] I can only join one channel per server.")
 							break
 						end
 					end
 				end
 				
-				if(not channelExists) then
-					msg.getChannel().sendMessage("[INFO] There is no channel called: \"".. args[1] .."\".\n")
+				if(args[1] == channel.getName() and not alreadyInChannel) then -- if there is a channel with name <arg>
+					if(not voiceChannels[k].isConnected()) then
+						voiceChannels[k].join()
+						connectedChannel = voiceChannels[k]
+						channelExists = true
+						break
+					end
 				end
-			elseif(#args >= 1) then
-				msg.getChannel().sendMessage("[INFO] I can only join one channel per server.")
-			else -- need one arg to specify channel to leave
-				msg.getChannel().sendMessage("[INFO] There are multiple voice channels.\n[INFO] Please specify the voice channel you would like me to join.\n[INFO] Usage: join <channel>")
 			end
-		else
-			msg.getChannel().sendMessage("[INFO] There are no voice channels on this server.")
+			
+			if(not channelExists) then
+				msg.getChannel().sendMessage("[INFO] There is no channel called: \"".. args[1] .."\".\n")
+			end
+		elseif(#args >= 1) then
+			msg.getChannel().sendMessage("[INFO] I can only join one channel per server.")
+		else -- need one arg to specify channel to leave
+			msg.getChannel().sendMessage("[INFO] There are multiple voice channels.\n[INFO] Please specify the voice channel you would like me to join.\n[INFO] Usage: join <channel>")
 		end
 	else
-		msg.getChannel().sendMessage("[INFO] Admin-only command.")
+		msg.getChannel().sendMessage("[INFO] There are no voice channels on this server.")
 	end
 	
 	msg.delete()
 end)
 
 command.add("leave", function(msg, args)
-	if(core.isAdmin(msg)) then
-		local voiceChannels = discord.getConnectedVoiceChannels()
+	local voiceChannels = discord.getConnectedVoiceChannels()
 
-		for i, channel in pairs(voiceChannels) do
-			channel.leave()
-		end
-		
-		connectedChannel = nil
-	else
-		msg.getChannel().sendMessage("[INFO] Admin-only command.")
+	for i, channel in pairs(voiceChannels) do
+		channel.leave()
 	end
+	
+	connectedChannel = nil
 	
 	msg.delete()
 end)
